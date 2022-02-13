5dbac8c01e8abe7e899c791663bff6133e49a0be
CynthiaOuma12673 <cynthia.ouma@student.moringaschool.com>
2022-02-13 15:05:21 +0300

    it will add the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GifsComponent } from './gifs/gifs.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { UserComponent } from './user/user.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GifsComponent,
+    NavbarComponent,
+    NotFoundComponent,
+    RepositoriesComponent,
+    UserComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
